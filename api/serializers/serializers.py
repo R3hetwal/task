36f988d092b83cde169b707ca9adc6008fd1ae82
R3hetwal <rakshya.bhetwal34@gmail.com>
2023-04-21 11:17:05 +0545

    first commit

---
+++ api/serializers/serializers.py
@@ -0,0 +1,7 @@
+# from rest_framework import serializers
+# from core.models import JSONData
+
+# class JSONDataSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = JSONData
+#         fields = ['title', 'file']
---
+++ api/viewsets/viewsets.py
@@ -0,0 +1,122 @@
+import csv
+from rest_framework.decorators import api_view
+from django.http import JsonResponse
+
+@api_view(['GET'])
+def get_data_for_Survey_news(request):
+    data = []
+    with open('scraped/Survey_news.csv') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            data.append(row)
+    return JsonResponse(data, safe=False)
+
+@api_view(['GET'])             
+def get_data_for_Survey_publications(request):
+    data = []
+    with open('scraped/Survey_publications.csv') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            data.append(row)
+    return JsonResponse(data, safe=False)
+
+
+@api_view(['GET'])             
+def get_data_for_अन्य_उपयोगी_सामग्री(request):
+    data = []
+    with open('scraped/anyaupayogisamagri.csv') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            data.append(row)
+    return JsonResponse(data, safe=False)
+
+
+@api_view(['GET'])             
+def get_data_for_डाउनलोड_अन्य(request):
+    data = []
+    with open('scraped/downloadanya.csv') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            data.append(row)
+    return JsonResponse(data, safe=False)
+
+@api_view(['GET'])             
+def get_data_for_डाउनलोड_ऐन_नियम(request):
+    data = []
+    with open('scraped/downloadyainniyam.csv') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            data.append(row)
+    return JsonResponse(data, safe=False)
+
+@api_view(['GET'])             
+def get_data_for_डाउनलोड_कार्यविधि(request):
+    data = []
+    with open('scraped/downloadkaryabidhi.csv') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            data.append(row)
+    return JsonResponse(data, safe=False)
+
+@api_view(['GET'])             
+def get_data_for_डाउनलोड_नियमावली(request):
+    data = []
+    with open('scraped/downloadniyamawali.csv') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            data.append(row)
+    return JsonResponse(data, safe=False)
+
+@api_view(['GET'])             
+def get_data_for_डाउनलोड_निर्देशिका(request):
+    data = []
+    with open('scraped/downloadnirdeshika.csv') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            data.append(row)
+    return JsonResponse(data, safe=False)
+
+@api_view(['GET'])             
+def get_data_for_बारम्बार_सोधिने_प्रश्नहरु(request):
+    data = []
+    with open('scraped/faq.csv') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            data.append(row)
+    return JsonResponse(data, safe=False)
+
+@api_view(['GET'])             
+def get_data_for_सहकारी_सङ्घ_संस्था_तथ्याङ्क(request):
+    data = []
+    with open('scraped/sahakarisanghsansthatathyanka.csv') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            data.append(row)
+    return JsonResponse(data, safe=False)
+
+@api_view(['GET'])             
+def get_data_for_सूचना(request):
+    data = []
+    with open('scraped/suchana.csv') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            data.append(row)
+    return JsonResponse(data, safe=False)
+
+@api_view(['GET'])             
+def get_data_for_सूचनाको_हक(request):
+    data = []
+    with open('scraped/suchanakohak.csv') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            data.append(row)
+    return JsonResponse(data, safe=False)
+
+@api_view(['GET'])             
+def get_data_for_nlc_सूचना(request):
+    data = []
+    with open('scraped/nlcnotice.csv') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            data.append(row)
+    return JsonResponse(data, safe=False)
