00bf9f43e0ab472c304da09d3e382f993633bdbc
R3hetwal <rakshya.bhetwal34@gmail.com>
2023-04-22 21:07:22 +0545

    update few pattern change

--- api/urls/urls.py
+++ api/urls/urls.py
@@ -1,20 +1,19 @@
 from django.contrib import admin
-from django.urls import path, include
-from api.viewsets.viewsets import get_data_for_Survey_publications, get_data_for_Survey_news, get_data_for_अन्य_उपयोगी_सामग्री, get_data_for_डाउनलोड_अन्य, get_data_for_डाउनलोड_ऐन_नियम, get_data_for_डाउनलोड_कार्यविधि, get_data_for_डाउनलोड_नियमावली, get_data_for_डाउनलोड_निर्देशिका, get_data_for_बारम्बार_सोधिने_प्रश्नहरु, get_data_for_सहकारी_सङ्घ_संस्था_तथ्याङ्क, get_data_for_सूचना, get_data_for_सूचनाको_हक, get_data_for_nlc_सूचना
-from rest_framework import routers
+from django.urls import path
+from api.viewsets.viewsets import get_data_for_Survey_publications, get_data_for_Survey_news, get_data_for_अन्य_उपयोगी_सामग्री, get_data_for_डाउनलोड_अन्य, get_data_for_डाउनलोड_ऐन_नियम, get_data_for_डाउनलोड_कार्यविधि, get_data_for_डाउनलोड_नियमावली, get_data_for_डाउनलोड_निर्देशिका, get_data_for_बारम्बार_सोधिने_प्रश्नहरु, get_data_for_सहकारी_सङ्घ_संस्था_तथ्याङ्क,  get_data_for_सूचनाको_हक, get_data_for_nlc_सूचना, get_data_for_nlc_डाउनलाेड
 
 urlpatterns = [
-    path('dos/survey_news', get_data_for_Survey_news),
-    path('dos/survey_publications', get_data_for_Survey_publications),
-    path('deoc/downloadsdetail/4/2017/17421968/', get_data_for_अन्य_उपयोगी_सामग्री),
-    path('deoc/downloadsdetail/5/2019/63273428/', get_data_for_सूचनाको_हक),
-    path('deoc/common', get_data_for_सूचना),
-    path('deoc/downloadsdetail/3/2017/76236913/', get_data_for_सहकारी_सङ्घ_संस्था_तथ्याङ्क),
-    path('dolma//office/dept/content/other-1659866558', get_data_for_डाउनलोड_अन्य),
-    path('dolma/office/dept/content/rules-regulations', get_data_for_डाउनलोड_ऐन_नियम),
-    path('dolma//office/dept/content/procedure-1635137665', get_data_for_डाउनलोड_कार्यविधि),
-    path('dolma/office/dept/content/regulation-1635137640', get_data_for_डाउनलोड_नियमावली),
-    path('dolma/office/dept/content/rules-1635137656', get_data_for_डाउनलोड_निर्देशिका),
-    path('merokitta/faq', get_data_for_बारम्बार_सोधिने_प्रश्नहरु),
-    path('nlc/सूचना', get_data_for_nlc_सूचना),
+    path('scrape-news', get_data_for_Survey_news),
+    path('scrape-publications', get_data_for_Survey_publications),
+    path('scrape-downloadsdetail/4/2017/17421968', get_data_for_अन्य_उपयोगी_सामग्री),
+    path('scrape-downloadsdetail/5/2019/63273428', get_data_for_सूचनाको_हक),
+    path('scrape-downloadsdetail/3/2017/76236913', get_data_for_सहकारी_सङ्घ_संस्था_तथ्याङ्क),
+    path('scrape-office/dept/content/other-1659866558', get_data_for_डाउनलोड_अन्य),
+    path('scrape-office/dept/content/rules-regulations', get_data_for_डाउनलोड_ऐन_नियम),
+    path('scrape-office/dept/content/procedure-1635137665', get_data_for_डाउनलोड_कार्यविधि),
+    path('scrape-office/dept/content/regulation-1635137640', get_data_for_डाउनलोड_नियमावली),
+    path('scrape-office/dept/content/rules-1635137656', get_data_for_डाउनलोड_निर्देशिका),
+    path('scrape-FAQ', get_data_for_बारम्बार_सोधिने_प्रश्नहरु),
+    path('scrape-suchana', get_data_for_nlc_सूचना),
+    path('scrape-download', get_data_for_nlc_डाउनलाेड),
 ]
--- api/viewsets/viewsets.py
+++ api/viewsets/viewsets.py
@@ -4,119 +4,311 @@ from django.http import JsonResponse
 
 @api_view(['GET'])
 def get_data_for_Survey_news(request):
+    survey_name = request.GET.get('for')
+
+    # Define a mapping from survey names to CSV file names
+    survey_file_map = {
+        'dos': 'scraped/scrape-news.csv',
+    }
+
+    # Check if the requested survey is valid
+    if survey_name not in survey_file_map:
+        return JsonResponse({'error': 'Invalid survey name. Missing "?for=parameters{dos}'})
+
+    # Scrape the data from the corresponding CSV file
     data = []
-    with open('scraped/Survey_news.csv') as file:
+    with open(survey_file_map[survey_name]) as file:
         reader = csv.DictReader(file)
         for row in reader:
             data.append(row)
+
+    # Return the scraped data as a JSON response
     return JsonResponse(data, safe=False)
 
-@api_view(['GET'])             
+@api_view(['GET'])
 def get_data_for_Survey_publications(request):
+    survey_name = request.GET.get('for')
+
+    # Define a mapping from survey names to CSV file names
+    survey_file_map = {
+        'dos': 'scraped/Survey_publications.csv'
+    }
+
+    # Check if the requested survey is valid
+    if survey_name not in survey_file_map:
+        return JsonResponse({'error': 'Invalid survey name. Missing "?for=parameters{dos}'})
+
+    # Scrape the data from the corresponding CSV file
     data = []
-    with open('scraped/Survey_publications.csv') as file:
+    with open(survey_file_map[survey_name]) as file:
         reader = csv.DictReader(file)
         for row in reader:
             data.append(row)
-    return JsonResponse(data, safe=False)
 
+    # Return the scraped data as a JSON response
+    return JsonResponse(data, safe=False)
 
 @api_view(['GET'])             
 def get_data_for_अन्य_उपयोगी_सामग्री(request):
+    survey_name = request.GET.get('for')
+
+    # Define a mapping from survey names to CSV file names
+    survey_file_map = {
+        'deoc': 'scraped/anyaupayogisamagri.csv'
+    }
+
+    # Check if the requested survey is valid
+    if survey_name not in survey_file_map:
+        return JsonResponse({'error': 'Invalid survey name. '})
+
+    # Scrape the data from the corresponding CSV file
     data = []
-    with open('scraped/anyaupayogisamagri.csv') as file:
+    with open(survey_file_map[survey_name]) as file:
         reader = csv.DictReader(file)
         for row in reader:
             data.append(row)
+
+    # Return the scraped data as a JSON response
     return JsonResponse(data, safe=False)
 
 
 @api_view(['GET'])             
 def get_data_for_डाउनलोड_अन्य(request):
+    survey_name = request.GET.get('for')
+
+    # Define a mapping from survey names to CSV file names
+    survey_file_map = {
+        'dolma': 'scraped/downloadanya.csv'
+    }
+
+    # Check if the requested survey is valid
+    if survey_name not in survey_file_map:
+        return JsonResponse({'error': 'Invalid survey name.'})
+
+    # Scrape the data from the corresponding CSV file
     data = []
-    with open('scraped/downloadanya.csv') as file:
+    with open(survey_file_map[survey_name]) as file:
         reader = csv.DictReader(file)
         for row in reader:
             data.append(row)
+
+    # Return the scraped data as a JSON response
     return JsonResponse(data, safe=False)
 
 @api_view(['GET'])             
 def get_data_for_डाउनलोड_ऐन_नियम(request):
+    survey_name = request.GET.get('for')
+
+    # Define a mapping from survey names to CSV file names
+    survey_file_map = {
+        'dolma': 'scraped/downloadyainniyam.csv'
+    }
+
+    # Check if the requested survey is valid
+    if survey_name not in survey_file_map:
+        return JsonResponse({'error': 'Invalid survey name.'})
+
+    # Scrape the data from the corresponding CSV file
     data = []
-    with open('scraped/downloadyainniyam.csv') as file:
+    with open(survey_file_map[survey_name]) as file:
         reader = csv.DictReader(file)
         for row in reader:
             data.append(row)
+
+    # Return the scraped data as a JSON response
     return JsonResponse(data, safe=False)
 
 @api_view(['GET'])             
 def get_data_for_डाउनलोड_कार्यविधि(request):
+    survey_name = request.GET.get('for')
+
+    # Define a mapping from survey names to CSV file names
+    survey_file_map = {
+        'dolma': 'scraped/downloadkaryabidhi.csv'
+    }
+
+    # Check if the requested survey is valid
+    if survey_name not in survey_file_map:
+        return JsonResponse({'error': 'Invalid survey name.'})
+
+    # Scrape the data from the corresponding CSV file
     data = []
-    with open('scraped/downloadkaryabidhi.csv') as file:
+    with open(survey_file_map[survey_name]) as file:
         reader = csv.DictReader(file)
         for row in reader:
             data.append(row)
+
+    # Return the scraped data as a JSON response
     return JsonResponse(data, safe=False)
 
-@api_view(['GET'])             
+@api_view(['GET'])                       
 def get_data_for_डाउनलोड_नियमावली(request):
+    survey_name = request.GET.get('for')
+
+    # Define a mapping from survey names to CSV file names
+    survey_file_map = {
+        'dolma': 'scraped/downloadniyamawali.csv'
+    }
+
+    # Check if the requested survey is valid
+    if survey_name not in survey_file_map:
+        return JsonResponse({'error': 'Invalid survey name.'})
+
+    # Scrape the data from the corresponding CSV file
     data = []
-    with open('scraped/downloadniyamawali.csv') as file:
+    with open(survey_file_map[survey_name]) as file:
         reader = csv.DictReader(file)
         for row in reader:
             data.append(row)
+
+    # Return the scraped data as a JSON response
     return JsonResponse(data, safe=False)
 
 @api_view(['GET'])             
 def get_data_for_डाउनलोड_निर्देशिका(request):
+    survey_name = request.GET.get('for')
+
+    # Define a mapping from survey names to CSV file names
+    survey_file_map = {
+        'dolma': 'scraped/downloadnirdeshika.csv'
+    }
+
+    # Check if the requested survey is valid
+    if survey_name not in survey_file_map:
+        return JsonResponse({'error': 'Invalid survey name.'})
+
+    # Scrape the data from the corresponding CSV file
     data = []
-    with open('scraped/downloadnirdeshika.csv') as file:
+    with open(survey_file_map[survey_name]) as file:
         reader = csv.DictReader(file)
         for row in reader:
             data.append(row)
+
+    # Return the scraped data as a JSON response
     return JsonResponse(data, safe=False)
 
 @api_view(['GET'])             
 def get_data_for_बारम्बार_सोधिने_प्रश्नहरु(request):
+    survey_name = request.GET.get('for')
+
+    # Define a mapping from survey names to CSV file names
+    survey_file_map = {
+        'merokitta': 'scraped/faq.csv'
+    }
+
+    # Check if the requested survey is valid
+    if survey_name not in survey_file_map:
+        return JsonResponse({'error': 'Invalid survey name.'})
+
+    # Scrape the data from the corresponding CSV file
     data = []
-    with open('scraped/faq.csv') as file:
+    with open(survey_file_map[survey_name]) as file:
         reader = csv.DictReader(file)
         for row in reader:
             data.append(row)
+
+    # Return the scraped data as a JSON response
     return JsonResponse(data, safe=False)
+
 
 @api_view(['GET'])             
 def get_data_for_सहकारी_सङ्घ_संस्था_तथ्याङ्क(request):
+    survey_name = request.GET.get('for')
+
+    # Define a mapping from survey names to CSV file names
+    survey_file_map = {
+        'doec': 'scraped/sahakarisanghsansthatathyanka.csv'
+    }
+
+    # Check if the requested survey is valid
+    if survey_name not in survey_file_map:
+        return JsonResponse({'error': 'Invalid survey name.'})
+
+    # Scrape the data from the corresponding CSV file
     data = []
-    with open('scraped/sahakarisanghsansthatathyanka.csv') as file:
+    with open(survey_file_map[survey_name]) as file:
         reader = csv.DictReader(file)
         for row in reader:
             data.append(row)
+
+    # Return the scraped data as a JSON response
     return JsonResponse(data, safe=False)
 
-@api_view(['GET'])             
-def get_data_for_सूचना(request):
-    data = []
-    with open('scraped/suchana.csv') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            data.append(row)
-    return JsonResponse(data, safe=False)
+# @api_view(['GET'])             
+# def get_data_for_सूचना(request):
+#     data = []
+#     with open('scraped/suchana.csv') as file:
+#         reader = csv.DictReader(file)
+#         for row in reader:
+#             data.append(row)
+#     return JsonResponse(data, safe=False)
 
 @api_view(['GET'])             
 def get_data_for_सूचनाको_हक(request):
+    survey_name = request.GET.get('for')
+
+    # Define a mapping from survey names to CSV file names
+    survey_file_map = {
+        'doec': 'scraped/suchanakohak.csv'
+    }
+
+    # Check if the requested survey is valid
+    if survey_name not in survey_file_map:
+        return JsonResponse({'error': 'Invalid survey name.'})
+
+    # Scrape the data from the corresponding CSV file
     data = []
-    with open('scraped/suchanakohak.csv') as file:
+    with open(survey_file_map[survey_name]) as file:
         reader = csv.DictReader(file)
         for row in reader:
             data.append(row)
+
+    # Return the scraped data as a JSON response
     return JsonResponse(data, safe=False)
+
 
 @api_view(['GET'])             
 def get_data_for_nlc_सूचना(request):
+    survey_name = request.GET.get('for')
+
+    # Define a mapping from survey names to CSV file names
+    survey_file_map = {
+        'doec': 'scraped/nlcnotice.csv'
+    }
+
+    # Check if the requested survey is valid
+    if survey_name not in survey_file_map:
+        return JsonResponse({'error': 'Invalid survey name.'})
+
+    # Scrape the data from the corresponding CSV file
     data = []
-    with open('scraped/nlcnotice.csv') as file:
+    with open(survey_file_map[survey_name]) as file:
         reader = csv.DictReader(file)
         for row in reader:
             data.append(row)
+
+    # Return the scraped data as a JSON response
+    return JsonResponse(data, safe=False)
+
+@api_view(['GET'])             
+def get_data_for_nlc_डाउनलाेड(request):
+    survey_name = request.GET.get('for')
+
+    # Define a mapping from survey names to CSV file names
+    survey_file_map = {
+        'doec': 'scraped/nlcdownloads.csv'
+    }
+
+    # Check if the requested survey is valid
+    if survey_name not in survey_file_map:
+        return JsonResponse({'error': 'Invalid survey name.'})
+
+    # Scrape the data from the corresponding CSV file
+    data = []
+    with open(survey_file_map[survey_name]) as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            data.append(row)
+
+    # Return the scraped data as a JSON response
     return JsonResponse(data, safe=False)
